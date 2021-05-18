8e9032a806280a65bf86977169abf50399a233b6
michal.studzinski <michal.studzinski@clarite.pl>
2021-05-18 17:41:37 +0200

    save to JSON

--- examples/gas.py
+++ examples/gas.py
@@ -3,11 +3,15 @@
 import time
 from enviroplus import gas
 import logging
+import json
+
+curr_time = time.localtime()
+curr_clock = time.strftime("%Y%m%d_%H%M%S_%Z%z", curr_time)
 
 logging.basicConfig(
     format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
     level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
+    datefmt='%Y%m%d_%H%M%S_%Z%z')
 
 logging.info("""gas.py - Print readings from the MICS6814 Gas sensor.
 
@@ -17,7 +21,11 @@ Press Ctrl+C to exit!
 
 try:
     while True:
+        current_time = time.localtime()
+        current_clock = time.strftime("%Y%m%d_%H%M%S_%Z%z", current_time)
         readings = gas.read_all()
+        with open('/opt/sensor/input/' +'GAS_' + current_clock + '.json', 'w') as outfile:
+            json.dump(readings, outfile)
         logging.info(readings)
         time.sleep(1.0)
 except KeyboardInterrupt:
