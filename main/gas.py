8ab16875a8f623c929d6caa75c39ee34fa660460
michal.studzinski <michal.studzinski@clarite.pl>
2021-05-18 20:45:33 +0200

    working gas

--- main/gas.py
+++ main/gas.py
@@ -1,9 +1,10 @@
-"""Read the MICS6814 via an ads1015 ADC"""
-
 import time
 import atexit
 import ads1015
 import RPi.GPIO as GPIO
+from os import close
+import time
+from datetime import datetime
 
 MICS6814_HEATER_PIN = 24
 MICS6814_GAIN = 6.144
@@ -135,3 +136,17 @@ def read_adc():
     """Return spare ADC channel value"""
     setup()
     return read_all().adc
+
+try:
+    while True:
+        current_time = datetime.now()
+        dataTimeZone = current_time.strftime('%Y-%m-%dT%H:%M:%S.%f%zZ')
+        current_clock = current_time.strftime("%Y%m%d_%H%M%S")
+        dataTime = current_time.strftime("%Y-%m-%dT%H:%M:%S")
+        readings = read_all()
+        with open('/opt/sensor/input/' +'GAS_' + current_clock + '.json', 'w') as outfile: 
+            outfile.write("{" +"\"dateTime\" : " +"\"" + dataTimeZone + "\"" + "," + str(readings) + "}")
+            outfile.close()
+        time.sleep(1.0)
+except KeyboardInterrupt:
+    pass
